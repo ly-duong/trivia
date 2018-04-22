0b38104b65b09448b6e30886ee62972ff76bc159
Arthur Cheung <arthur.cheung@accenture.com>
2018-04-23 09:31:44 +1000

    Created trivia component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home';
+import { TriviaOptionsComponent } from './trivia-options/trivia-options.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TriviaOptionsComponent
   ],
   imports: [
     BrowserModule,
