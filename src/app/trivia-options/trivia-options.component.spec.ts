6f604260f4781d448911db1b8819ef654b689dc9
Arthur Cheung <arthur.cheung@accenture.com>
2018-04-23 14:13:15 +1000

    Added tests for bindings to spec for trivia-options-component.

--- src/app/trivia-options/trivia-options.component.spec.ts
+++ src/app/trivia-options/trivia-options.component.spec.ts
@@ -29,21 +29,80 @@ describe('TriviaOptionsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should get number of questions from input with name "options-number"', () => {
+  it('should bind numOfQuestions property with input with name "options-number"', () => {
     // When stable makes sure test is run after:
     // 1) Bindings are ready
     // 2) Any default values are loaded
     fixture.whenStable().then(() => {
+      const TEST_VALUE_1 = 30;
       let control = fixture.debugElement.query(By.css('.question-number'));
       let element = control.nativeElement;
-      element.value = 30;
+      element.value = TEST_VALUE_1;
       element.dispatchEvent(new Event('input'));
-      expect(Number(element.value)).toBe(component.options.numQuestions);
+      expect(component.options.numQuestions).toBe(TEST_VALUE_1);
 
       // Put in a second update for good measure
-      element.value = 50;
+      const TEST_VALUE_2 = 50
+      element.value = TEST_VALUE_2;
       element.dispatchEvent(new Event('input'));
-      expect(Number(element.value)).toBe(component.options.numQuestions);
+      expect(component.options.numQuestions).toBe(TEST_VALUE_2);
+    }).catch(error => {
+      expect(error).toBeFalsy();
+    });;
+  });
+
+  it('should bind difficulty with the checkboxes named "options-difficulty"', () => {
+    // When stable makes sure test is run after:
+    // 1) Bindings are ready
+    // 2) Any default values are loaded
+    fixture.whenStable().then(() => {
+      // Before we start, value should be nothing
+      expect(component.options.difficulty).toBeFalsy();
+      const TEST_VALUE = 'easy';
+      let control = fixture.debugElement.query(By.css(`.options-difficulty input[value=${TEST_VALUE}]`));
+      let element = control.nativeElement;
+      element.dispatchEvent(new Event('change'));
+      expect(component.options.difficulty).toBe(TEST_VALUE);
+    }).catch(error => {
+      expect(error).toBeFalsy();
+    });
+  });
+
+  it('should bind category with the select named "options-category"', () => {
+    // When stable makes sure test is run after:
+    // 1) Bindings are ready
+    // 2) Any default values are loaded
+    fixture.whenStable().then(() => {
+      expect(component.options.category).toBeFalsy();
+  
+      let control = fixture.debugElement.query(By.css('select[name=options-category]'));
+      let element = control.nativeElement;
+
+      const TEST_VALUE = element.querySelector('option:last-child').value; // Get last option in select
+      element.value = TEST_VALUE;
+      element.dispatchEvent(new Event('change'));
+      expect(component.options.category).toBe(TEST_VALUE);
+    }).catch(error => {
+      expect(error).toBeFalsy();
+    });
+  });
+
+  it('should bind type with the select named "options-type"', () => {
+    // When stable makes sure test is run after:
+    // 1) Bindings are ready
+    // 2) Any default values are loaded
+    fixture.whenStable().then(() => {
+      expect(component.options.questionType).toBeFalsy();
+  
+      let control = fixture.debugElement.query(By.css('select[name=options-type]'));
+      let element = control.nativeElement;
+
+      const TEST_VALUE = element.querySelector('option:last-child').value; // Get last option in select
+      element.value = TEST_VALUE;
+      element.dispatchEvent(new Event('change'));
+      expect(component.options.questionType).toBe(TEST_VALUE);
+    }).catch(error => {
+      expect(error).toBeFalsy();
     });
   });
 });
