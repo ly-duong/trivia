dce7d9c627a46e403ee75159208b5254c12c89ba
Arthur Cheung <arthur.cheung@accenture.com>
2018-04-23 13:32:39 +1000

    Created unit test for number of questions binding.

--- src/app/trivia-options/trivia-options.component.spec.ts
+++ src/app/trivia-options/trivia-options.component.spec.ts
@@ -1,8 +1,9 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TriviaOptions } from '../core/models';
 
 import { TriviaOptionsComponent } from './trivia-options.component';
+import { By } from '@angular/platform-browser';
 
 describe('TriviaOptionsComponent', () => {
   let component: TriviaOptionsComponent;
@@ -10,12 +11,12 @@ describe('TriviaOptionsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TriviaOptionsComponent ],
+      declarations: [TriviaOptionsComponent],
       imports: [
         FormsModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -26,5 +27,23 @@ describe('TriviaOptionsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get number of questions from input with name "options-number"', () => {
+    // When stable makes sure test is run after:
+    // 1) Bindings are ready
+    // 2) Any default values are loaded
+    fixture.whenStable().then(() => {
+      let control = fixture.debugElement.query(By.css('.question-number'));
+      let element = control.nativeElement;
+      element.value = 30;
+      element.dispatchEvent(new Event('input'));
+      expect(Number(element.value)).toBe(component.options.numQuestions);
+
+      // Put in a second update for good measure
+      element.value = 50;
+      element.dispatchEvent(new Event('input'));
+      expect(Number(element.value)).toBe(component.options.numQuestions);
+    });
   });
 });
