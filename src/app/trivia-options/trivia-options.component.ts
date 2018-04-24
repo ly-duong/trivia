2c97398ab8b3cd04c48484a76aaec7e2a4178828
brendan.lee.reinhart <brendan.lee.reinhart@accenture.com>
2018-04-24 11:47:05 +1000

    Added getQuestions method in triviaOptionsComponent for capturing user options.

--- src/app/trivia-options/trivia-options.component.ts
+++ src/app/trivia-options/trivia-options.component.ts
@@ -6,17 +6,45 @@ import { TriviaService } from '../core/services';
 @Component({
   selector: 'app-trivia-options',
   templateUrl: './trivia-options.component.html',
-  styleUrls: ['./trivia-options.component.css']
+  styleUrls: ['./trivia-options.component.css'],
+  providers: [TriviaService]
 })
 export class TriviaOptionsComponent implements OnInit {
   public trivia_categories;
   options: TriviaOptions;
+
+  // Suggest redefining <TriviaOptions> to match API param names exactly, and deleting callOptions:
+  // redefine param names to match API:
+  callOptions: {
+    amount?: number,
+    category?: string,
+    difficulty?: string,
+    type?: string
+  };
+
   constructor(private triviaService: TriviaService) { }
 
   ngOnInit() {
     this.options = <TriviaOptions> {};
     this.triviaService.getCategories().subscribe(
-    response => this.trivia_categories = response);
+      response => this.trivia_categories = response);
+    this.options = <TriviaOptions> {};
+  }
+
+  getQuestions() {
+    // If <TriviaOptions> can be redefined to match API params, eliminate below and just return this.options;
+    this.callOptions = {
+      amount: this.options.numQuestions,
+      category: this.options.category,
+      difficulty: this.options.difficulty,
+      type: this.options.questionType
+    };
+
+    // Nifty trick to eliminate undefined variables from the Options object:
+    this.callOptions = JSON.parse(JSON.stringify(this.callOptions));
+
+    console.log('user options at button click: ', this.callOptions);
+    return this.callOptions;
   }
 
 }
