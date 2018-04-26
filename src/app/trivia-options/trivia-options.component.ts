090db9db4fd56ba9c31b6bfa9708009986b5ba65
Arthur Cheung <arthur.cheung@accenture.com>
2018-04-26 11:02:31 +1000

    Fixed bug where TriviaService was accidentally removed as provider from HomeComponent accidnetally.

--- src/app/trivia-options/trivia-options.component.ts
+++ src/app/trivia-options/trivia-options.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { TriviaOptions, TriviaCategory } from '../core/models';
 import { NgForm } from '@angular/forms';
+import { TriviaService } from '../core/services';
 
 @Component({
   selector: 'app-trivia-options',
   templateUrl: './trivia-options.component.html',
   styleUrls: ['./trivia-options.component.css'],
+  providers: [TriviaService],
 })
 export class TriviaOptionsComponent implements OnInit {
   public trivia_categories;
@@ -14,10 +16,12 @@ export class TriviaOptionsComponent implements OnInit {
   @Output()
   optionsModel: EventEmitter<TriviaOptions> = new EventEmitter<TriviaOptions>();
 
-  constructor() { }
+  constructor(private triviaService: TriviaService) { }
 
   ngOnInit() {
     this.options = <TriviaOptions> {};
+    this.triviaService.getCategories().subscribe(
+      response => this.trivia_categories = response);
   }
 
   getOptions() {
