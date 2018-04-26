a395cfe24b447baa2107f1c60925f2f9b1236bfb
Duong <ly.duong@accenture.com>
2018-04-26 11:19:46 +1000

    Created questions model.

---
+++ src/app/core/models/questions.model.ts
@@ -0,0 +1,8 @@
+export interface TriviaQuestions {
+    question: string;
+    correctAnswer: string;
+    incorrectAnswers: string[];
+    category: string;
+    difficulty: string;
+    type: string;
+}
